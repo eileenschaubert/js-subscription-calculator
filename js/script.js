91e4876e3a8888149fba99026385bba5d95d0083
eileenschaubert <eileen.schaubert@gmail.com>
2023-11-07 22:10:51 -0600

    cleanup after comparison to solution code

--- js/script.js
+++ js/script.js
@@ -7,26 +7,26 @@ var subDuration = Number("1");
 
 subTypeElement.addEventListener("change", function (e) {
     subType = e.target.value;
-    if (subType === "basic"){
-        // console.log(subType);
-    } else if (subType === "standard"){
-        // console.log(subType);
-    } else if (subType === "premium"){
-        // console.log(subType);
-    }
+    // if (subType === "basic"){
+    //     console.log(subType);
+    // } else if (subType === "standard"){
+    //     console.log(subType);
+    // } else if (subType === "premium"){
+    //     console.log(subType);
+    // }
     updateSubscriptionDiv();
 });
 
-// To ensure the value is a number data type, wrap the reassigned value with Number(). Add later
+// To ensure the value is a number data type, wrap the reassigned value with Number(). I had done this in the intitial declaration.
 subDurationElement.addEventListener("change", function (e) {
-    subDuration = e.target.value;
-    if (subDuration === "1"){
-        // console.log(subDuration);
-    } else if (subDuration === "6"){
-        // console.log(subDuration);
-    } else if (subDuration === "12"){
-        // console.log(subDuration);
-    }
+    subDuration = Number(e.target.value);
+    // if (subDuration === "1"){
+    //     console.log(subDuration);
+    // } else if (subDuration === "6"){
+    //     console.log(subDuration);
+    // } else if (subDuration === "12"){
+    //     console.log(subDuration);
+    // }
     updateSubscriptionDiv();
 });
 
